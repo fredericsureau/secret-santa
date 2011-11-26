f57f8ac2a629edd096dfa896060ae9b6f886abf0
Alexis Metaireau <alexis@notmyidea.org>
2011-11-26 17:09:56 +0100

    made some changes to make the soft configurable easily

---
+++ README.rst
@@ -0,0 +1,13 @@
+Pour savoir qui offre un cadeau à qui !
+#######################################
+
+`Nouel` est un petit logiciel qui permet de, en fonction d'une liste de personne, 
+savoir qui offrira un cadeau à qui. Entrez la liste de personnes, leurs adresses email et "pouf, c'est tout" !
+
+Vous avez à rentrer les informations dans un fichier de configuration (par
+default, le fichier `config.py`)
+
+
+Usage::
+
+    $ ./nouel.py [config.py]
---
+++ config.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+MAIL_HOST = 'smtp.gmail.com' # if you want to use gmail
+MAIL_PORT = 587
+MAIL_USERNAME = 'your.name@gmail.com'
+MAIL_PASSWORD = 'yourpass'
+
+EMAILS = {
+    'Pierre': 'pierre@test.com',
+    'Marie': 'marie@test.com',
+    'Jean': 'jean@test.com',
+    'Claire': 'claire@test.com',
+}
+    
+COUPLES = (('Pierre','Marie'),('Jean',),('Claire',))
+SENDER = '"Le Père-Noël" <yourmail@test.com>'
+
+CONTENT = """Cher(e) %s,
+
+Cette année j'ai eu la chance d'échapper à la grippe de l'année dernière,
+cependant j'ai pris un peu de retard dans mon travail. Comme je sais que dans
+votre famille vous êtes toujours prêts à aider et que vous aimez beaucoup faire
+les magasins, j'ai demandé à mes lutins de confectionner un petit logiciel pour
+répartir les rôles entre vous sans que personne ne sache qui offre à qui.
+Eh oui, c'est qu'ils se mettent à l'informatique mes lutins !
+
+Je serais donc ravi si tu pouvais m'aider pour offrir un cadeau à %s.
+
+Ah oui, aussi, envoie-moi vite ta liste !
+Allez ! Bybye !
+
+Le Père-Noël.
+"""
